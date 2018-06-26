fa312326a08799e1a5f1f0429b746afa0bed16de
Grace Govan <ggovan11@outlook.com>
2018-06-26 14:11:18 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,10 +2,12 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   get '/name' do
-  ""
+   "My name is __" 
 end
   
-  
+   get '/hometown' do
+   "My name is __" 
+end
   
   
   
